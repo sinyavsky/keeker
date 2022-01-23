6b7c2d767311af4b67887ee57f2eb973f335385f
Alexander Sinyavsky <inbox@sinyavsky.com>
2022-01-23 22:00:35 +0300

    refactoring

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,15 +4,16 @@ if (process.env.NODE_ENV === 'development') {
   require('../index.html');
 }
 
-import Api from '../components/Api.js';
-import Parser from '../components/Parser.js';
+import getSourceTransactions from '../components/getSourceTransactions.js';
+import getTransactionBaseData from '../components/getTransactionBaseData.js';
+import HeadingGenerator from '../components/HeadingGenerator';
 
 import EntranceForm from '../components/EntranceForm.js';
 import TransactionsList from '../components/TransactionsList.js';
 import Transaction from '../components/Transaction.js';
 
 
-const api = new Api();
+
 const entranceForm = new EntranceForm({
   input: '.entrance__input',
   button: '.entrance__button'
@@ -21,29 +22,59 @@ const entranceForm = new EntranceForm({
 
 const transactionsList = new TransactionsList('.transactions__list');
 
+const headingGenerator = new HeadingGenerator();
+
 document.querySelector('.entrance__form').addEventListener('submit', async function (e) {
   e.preventDefault();
   const account = this.querySelector('.entrance__input').value;
   entranceForm.disableInput();
 
-  const transactions = await api.getAccountTransactions(account, 100);
-  const parser = new Parser();  
+  const transactions = await getSourceTransactions(account, 20);
+
 
   if(transactions.length > 0) {
     transactionsList.clear();
     transactions.forEach((item) => {      
-      const trxData = parser.getTransactionData(item, account);      
-      const trx = new Transaction(trxData);     
-      transactionsList.renderTransaction(trx.createHtmlElement({
+      const trxBaseData = getTransactionBaseData(item, account);
+      const trx = new Transaction(trxBaseData);
+      let trxElement = trx.createHtmlElement({
+        // selectors
         template: '.transaction-template',
+        transaction: '.transaction',
+        icon: '.transaction__icon',
         heading: '.transaction__heading',
         hash: '.transaction__hashlink',
         time: '.transaction__time',
         button: '.transaction__more',
         source: '.transaction__source',
-        sourceVisible: 'transaction__source_visible'
-      }));
+        // classes
+        iconPicClass: 'transaction__icon-picture',
+        sourceVisibleClass: 'transaction__source_visible',
+        trxTypeFunctionClass: 'transaction_type_function',
+        trxTypeReceiveClass: 'transaction_type_receive',
+        trxTypeSendClass: 'transaction_type_send',
+        trxTypeAddKeyClass: 'transaction_type_add-key',
+        trxTypeDeleteKeyClass: 'transaction_type_delete-key',
+        trxTypeDeployClass: 'transaction_type_deploy',
+        trxTypeStakeClass: 'transaction_type_stake',
+        trxTypeCreateAccountClass: 'transaction_type_create-account',
+        trxTypeDeleteAccountClass: 'transaction_type_delete-account',
+      });
+      
+      transactionsList.renderTransaction(trxElement);
+
+      if(trx.isFunctionCall()) {        
+        trxElement = document.querySelector('.transactions__list .transaction:last-child'); // weird, I should refactor it
+        headingGenerator.push({
+          headingElement: trxElement.querySelector('.transaction__heading'),
+          iconElement: trxElement.querySelector('.transaction__icon'),
+          trx: item,
+          account: account
+        });
+      }
     });
+
+    headingGenerator.generateHeadings();
   } else {
     transactionsList.renderEmptyResult();
   }
