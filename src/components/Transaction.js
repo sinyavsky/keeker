75986af07f5b510a118195bc1acf374e97b17888
Alexander Sinyavsky <inbox@sinyavsky.com>
2022-01-23 22:00:12 +0300

    add icon, transaction type classes and isFunctionCall

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,10 +1,12 @@
 import { ACTION_KIND, ACTION_DIRECTION } from '../utils/constants.js';
+import { formatDateFromNanoseconds } from '../utils/format.js';
 import { prettyPrintJson } from 'pretty-print-json';
 
 export default class Transaction {
 
   constructor(data) {
     this._heading = data.heading;
+    this._iconSrc = data.iconSrc;
     this._actionKind = data.actionKind;
     this._actionDirection = data.actionDirection;
     this._hash = data.hash;
@@ -13,38 +15,42 @@ export default class Transaction {
     this._source = data.source;
   }
 
+  isFunctionCall() {
+    return this._actionKind === ACTION_KIND.FUNCTION_CALL;
+  }
+
   createHtmlElement = (sel) => {
     this._html = document.querySelector(sel.template).content.cloneNode(true);  
-    const transaction = this._html.querySelector('.transaction');
+    const transaction = this._html.querySelector(sel.transaction);
     switch(this._actionKind) {
       case ACTION_KIND.FUNCTION_CALL:
-        
+        transaction.classList.add(sel.trxTypeFunctionClass);
       break;
       case ACTION_KIND.TRANSFER:
         if(this._actionDirection === ACTION_DIRECTION.IN) {
-          transaction.classList.add('transaction_type_receive');
+          transaction.classList.add(sel.trxTypeReceiveClass);
         }
         else if(this._actionDirection === ACTION_DIRECTION.OUT) {
-          transaction.classList.add('transaction_type_send');
+          transaction.classList.add(sel.trxTypeSendClass);
         }
       break;
       case ACTION_KIND.ADD_KEY:
-       
+        transaction.classList.add(sel.trxTypeAddKeyClass);
       break;
       case ACTION_KIND.DELETE_KEY:
-  
+        transaction.classList.add(sel.trxTypeDeleteKeyClass);
       break;
       case ACTION_KIND.DEPLOY_CONTRACT:
-
+        transaction.classList.add(sel.trxTypeDeployClass);
       break;
       case ACTION_KIND.STAKE:
-
+        transaction.classList.add(sel.trxTypeStakeClass);
       break;
       case ACTION_KIND.CREATE_ACCOUNT:
-
+        transaction.classList.add(sel.trxTypeCreateAccountClass);
       break;
       case ACTION_KIND.DELETE_ACCOUNT:
-
+        transaction.classList.add(sel.trxTypeDeleteAccountClass);
       break;      
     }
 
@@ -52,27 +58,31 @@ export default class Transaction {
     const hash = this._html.querySelector(sel.hash);
     const time = this._html.querySelector(sel.time);
     const source = this._html.querySelector(sel.source);
-
+    
     heading.insertAdjacentHTML('afterbegin', this._heading);
 
     hash.href += this._hash;
     hash.title = 'View at explorer.near.org';
     hash.textContent = this._hash;
 
-    time.textContent = new Date(this._blockTimestamp / 1000000); // blockTimestamp in nanoseconds
+    time.textContent = formatDateFromNanoseconds(this._blockTimestamp); // blockTimestamp in nanoseconds
 
     const button = this._html.querySelector(sel.button);
     button.addEventListener('click', () => {
-      if(source.classList.contains(sel.sourceVisible)) {
+      if(source.classList.contains(sel.sourceVisibleClass)) {
         button.textContent = 'Show source';
       }
       else {
         button.textContent = 'Hide source';
       }
-      source.classList.toggle(sel.sourceVisible);
+      source.classList.toggle(sel.sourceVisibleClass);
     });
 
     source.insertAdjacentHTML('afterbegin', prettyPrintJson.toHtml(this._source));
+
+    if(this._iconSrc) {
+      this._html.querySelector(sel.icon).innerHTML = `<img src="${this._iconSrc}" alt="" class="${sel.iconPicClass}">`;
+    }
 
     return this._html;
   }
