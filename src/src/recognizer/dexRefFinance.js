71fd9bf39ece4363a1ef6772c9cbf81daf082cdf
Alexander Sinyavsky <inbox@sinyavsky.com>
2022-02-13 21:15:14 +0300

    add withdraw for Ref.finance

--- src/src/recognizer/dexRefFinance.js
+++ src/src/recognizer/dexRefFinance.js
@@ -8,7 +8,8 @@ export default async function dexRefFinance(parser) {
   if(functionCallReceiver !== 'v2.ref-finance.near') {   
     return false;
   } 
-  
+  const method = parser.getFunctionCallMethod();
+
   const res = {
     iconSrc: iconRefFinance,
     iconAlt: 'Ref.finance',
@@ -16,25 +17,26 @@ export default async function dexRefFinance(parser) {
       section: FILTER_SECTION.OTHER,
       element: 'Ref.finance',
     },
+    heading: `Call method ${method} from the contract ${parser.getFunctionCallReceiver()}`,
   };
-
-  const method = parser.getFunctionCallMethod();  
+  
+  const argsJson = parser.getArgsJson();
   if(method === 'swap') {
-    const actions = parser.getArgsJson().actions;
+    const actions = argsJson.actions;
     const actionsArr = await actions.reduce(async (prev, cur) => {
       const metaIn = await contractApi.ft_metadata(cur.token_in);
       const metaOut = await contractApi.ft_metadata(cur.token_out);
       prev.push(`${formatTokenAmount(cur.amount_in, metaIn.decimals)} ${formatTokenName(metaIn.symbol, metaIn.name)} for ${formatTokenAmount(cur.min_amount_out, metaOut.decimals)} ${formatTokenName(metaOut.symbol, metaOut.name)}`);
       return prev;
     }, []);
-    return {
-      ...res,
-      heading: `Swap ${actionsArr.join(', ')} at Ref.finance`,
-    };
+    res.heading = `Swap ${actionsArr.join(', ')} at Ref.finance`;
+
   }
 
-  return {
-    ...res,
-    heading: `Call method ${method} from the contract ${parser.getFunctionCallReceiver()}`,
-  };
+  else if(method === 'withdraw') {
+    const metadata = await contractApi.ft_metadata(argsJson.token_id);
+    res.heading = `Withdraw ${formatTokenAmount(argsJson.amount, metadata.decimals)} ${formatTokenName(metadata.symbol, metadata.name)} from Ref.finance`;
+  }
+
+  return res;
 }
