5ff373f5d9e44776011983e68ec336000380ee15
Alexander Sinyavsky <inbox@sinyavsky.com>
2022-02-14 19:28:46 +0300

    update build

--- src/src/api/getSourceTransactions.js
+++
@@ -1,9 +0,0 @@
-
-export default async function getSourceTransactions(account, limit) {
-  limit = parseInt(limit);
-  if(limit < 1) {
-    limit = 1;
-  }
-  const response = await fetch(`https://helper.mainnet.near.org/account/${account}/activity?limit=${limit}`);
-  return await response.json();
-}
--- src/src/utils/utils.js
+++
@@ -1,3 +0,0 @@
-export function getParasNftUrl(id) {
-  return `https://paras.id/token/x.paras.near::${id}`;
-}
